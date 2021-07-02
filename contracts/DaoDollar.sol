a64654099aae492ebec5bdae9ebcdc8362d001f4
hephyrius <20884880+Hephyrius@users.noreply.github.com>
2021-07-02 19:26:55 +0100

    Update DaoDollar.sol

--- contracts/DaoDollar.sol
+++ contracts/DaoDollar.sol
@@ -4,6 +4,28 @@ DaoDollar ☯Dollar will be BUSL-1.1 initially so that we have the chance to dev
 */
 pragma solidity ^0.8.0;
 
-contract DaoDollar {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract DaoDollar is ERC20, Ownable {
+
+    address public minter;
+
+    constructor() ERC20("Dao Dollar", "dUSD") {
+        minter = msg.sender;
+        emit UpdateMinter(newMinter);
+    }
+
+    function mint(uint256 amount) public {
+        require(msg.sender == minter, "☯Dollar : NOT THE MINTER");
+        _mint(minter, amount);
+    }
+
+    function updateMinter(address newMinter) public onlyOwner {
+        minter = newMinter;
+        emit UpdateMinter(newMinter);
+    }
+
+    event UpdateMinter(address newMinter);
 }
