d29b87cad47d7dc0dedba7db476f8c35187aa663
hephyrius <20884880+Hephyrius@users.noreply.github.com>
2021-07-02 19:08:59 +0100

    initial setup of contracts + BUSL licenses

---
+++ contracts/DollarParams.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BUSL-1.1
+/*
+DaoDollar ☯Dollar will be BUSL-1.1 initially so that we have the chance to develop/launch the project without worrying about copycats.
+*/
+pragma solidity ^0.8.0;
+
+contract DollarParams {
+
+}
